d46d451381aa9fc01e5bab9adb4a0d80e3f06111
TheresNoTime <sam@theresnotime.co.uk>
2024-05-09 11:15:04 +0100

    update example config

--- config.example.py
+++ config.example.py
@@ -1,4 +1,5 @@
 API_URL = ""
 ACCESS_TOKEN = ""
 RSS_FEED = "https://www.wikimediastatus.net/history.rss"
+STATUS_API = "https://www.wikimediastatus.net/api/v2/incidents.json"
 STATE_FILE = "latest.txt"
